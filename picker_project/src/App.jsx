e53cc42a8c157c2219b1a874f15a6ccc5a6ef046
Léa Pollet <lea.pollet@epitech.eu>
2023-02-02 13:26:04 +0100

    [~] First picker version

--- picker_project/src/App.jsx
+++ picker_project/src/App.jsx
@@ -1,8 +1,20 @@
 import { DisplatyDifficulty } from "./DisplayDifficulty/DisplayDifficulty";
+import { useState } from "react";
+import { MenuList } from "./MenuList/MenuList";
+import s from "./style.module.css";
 
 export function App() {
+  const [currentDifficulty, setCurrentDifficulty] = useState("");
+
+  function updateDifficulty(difficulty) {
+    setCurrentDifficulty(difficulty);
+  }
   return <div>
-    <DisplatyDifficulty difficulty = "Low"/>
+    <h1 className={s.title}>Select your React Difficulty!</h1>
+    <div className={s.container}>
+      <MenuList onItemClick={updateDifficulty} difficulty = {currentDifficulty} />
+      <DisplatyDifficulty difficulty = {currentDifficulty} />
+    </div>
   </div>;
 }
 
